05746f628d831d15ca96e6736650b12d15e5ba23
GK Sir <hellofcode@hell.you>
2021-07-06 14:49:24 +0530

    Tue06Jul202102:49:24PM+0530

---
+++ HelloWorld.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+
+</head>
+<body>
+	<?php
+
+			echo "<h1 style=color:red;>Hello World</h1>";
+			echo "Bye<br>";
+
+			$a = "Denis"; // string data
+
+			echo "\$a = $a<br/>"; # $a = Denis
+
+
+			echo "<br>".var_dump($a); # string
+
+			$a = 10; // int data
+
+			echo "<br>".var_dump($a); # int 
+
+	?>
+</body>
+</html>
