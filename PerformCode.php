b0263b8430e59c71a5661fe134b4780ff36d6bd2
GK Sir <hellofcode@hell.you>
2021-07-08 14:30:33 +0530

    Thu08Jul202102:30:33PM+0530

---
+++ PerformCode.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="MyCss.css">
+</head>
+<body>
+		<div style="background: linear-gradient(to right,#505050,#707070);">
+
+			<?php
+
+				$a = 34;
+				$b = 16;
+
+				$c = $a + $b;
+				echo "$a + $b = $c<br/>";
+
+				$c = $a - $b;
+				echo "$a - $b = $c<br/>";
+
+				$c = $a * $b;
+				echo "$a * $b = $c<br/>";
+
+				$c = $a / $b;
+				echo "$a / $b = $c<br/>";
+
+				$c = $a % $b;
+				echo "$a % $b = $c<br/>"
+
+			?>
+		</div>
+</body>
+</html>
