f67b49b8b6a82753e182964b447130f3e0552261
GK Sir <hellofcode@hell.you>
2021-07-13 14:06:32 +0530

    Tue13Jul202102:06:32PM+0530

---
+++ PracticalCode.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="MyCss.css">
+	<link rel="icon"  href="code.png">
+</head>
+<body>
+	<?php 
+			echo "Hello";
+
+
+			for($a = 0;$a<50;$a++)
+				echo "&nbsp";
+			echo "World<br>";
+			echo "<hr>";
+
+			//THis is COmment
+			# code is not for noobs
+			/*
+				multiline comment
+
+			*/
+
+			echo "Line 1<br>";
+			echo "Line 2<br>";
+			echo "Line 3<br>";
+			
+
+	?>
+
+
+</body>
+</html>
