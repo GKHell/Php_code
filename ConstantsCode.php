d6df3b8ed1272e53c6b8173a097c870184b55a97
GK Sir <hellofcode@hell.you>
2021-07-20 14:38:49 +0530

    Tue20Jul202102:38:49PM+0530

--- ConstantsCode.php
+++ ConstantsCode.php
@@ -10,16 +10,41 @@
 <body>
 		<?php
 
-			$a = 10; //variable
+			// $a = 10; //variable
 
-			$a = 50;
-			echo "value of a : $a<hr>";
-			define("A", 300);  //constant 1
-			// A = 400;
-			echo "value of A : ".A."<hr>";
+			// $a = 50;
+			// echo "value of a : $a<hr>";
+			// define("A", 300);  //constant 1
+			// // A = 400;
+			// echo "value of A : ".A."<hr>";
 
-			const CODE = 130;  // constant 2
-			echo "value of CODE : ".CODE;
-			?>
+			// const CODE = 130;  // constant 2
+			// echo "value of CODE : ".CODE;
+
+
+			echo "LINE ".__LINE__."<br>";
+			echo "FILE ".__FILE__."<br>";
+			function Say()
+			{
+
+					echo "<i style=color:blue;>FUNCTION ".__FUNCTION__."<br>";
+						// code...
+			}
+			say();
+
+			//---------------------
+			class Code{
+				function tell()
+				{
+						echo "METHOD ".__METHOD__."<br>";
+						echo "<i style=color:red;>class ".__CLASS__."<br>";
+
+
+				}
+			}
+
+			$obj = new Code();
+			$obj->tell();
+				?>
 </body>
 </html>
