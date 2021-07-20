3eaf4edec412af70fd039b6bba7222339aa6c43c
GK Sir <hellofcode@hell.you>
2021-07-20 14:28:54 +0530

    Tue20Jul202102:28:54PM+0530

---
+++ ConstantsCode.php
@@ -0,0 +1,25 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="MyCss.css">
+</head>
+<body>
+		<?php
+
+			$a = 10; //variable
+
+			$a = 50;
+			echo "value of a : $a<hr>";
+			define("A", 300);  //constant 1
+			// A = 400;
+			echo "value of A : ".A."<hr>";
+
+			const CODE = 130;  // constant 2
+			echo "value of CODE : ".CODE;
+			?>
+</body>
+</html>
